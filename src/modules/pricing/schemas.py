a275af66472612fb62017f41dbe08a6a3e50f112
100kgtrotila <hirodevi192@gmail.com>
2026-02-19 16:08:40 +0200

    chore: Fix typos, update field types, and improve schema consistency in pricing module

--- src/modules/pricing/schemas.py
+++ src/modules/pricing/schemas.py
@@ -1,7 +1,7 @@
-from uuid import UUID
 from decimal import Decimal
+from typing import NewType
+from uuid import UUID
 
-from mypy.types import NewType
 from pydantic import BaseModel, Field, AwareDatetime
 
 from src.common.schemas import ORMBaseSchema
@@ -20,7 +20,7 @@ class PricingCreate(PricingBase):
     pass
 
 class PricingUpdate(BaseModel):
-    name: str  = Field(..., min_length=3, max_length=64, description="Pricing name")
+    name: str | None  = Field(default=None, min_length=3, max_length=64, description="Pricing name")
 
 class PricingResponse(PricingBase, ORMBaseSchema):
     id: PricingId
@@ -32,19 +32,19 @@ class PricingItemBase(BaseModel):
     pricing_id: PricingId = Field(...)
     seat_type_id: SeatTypeId = Field(...)
     day_of_week: int = Field(ge=1, le=7, description="Day of week")
-    star_time: AwareDatetime = Field(...)
+    start_time: AwareDatetime = Field(...)
     end_time: AwareDatetime = Field(...)
 
 class PricingItemCreate(PricingItemBase):
     pass
 
 class PricingItemUpdate(BaseModel):
-    price: Decimal = Field(default=None, gt=1, description="Price")
-    pricing_id: PricingId = Field(default=None)
-    seat_type_id: SeatTypeId = Field(default=None)
-    day_of_week: int = Field(default=None, ge=1, le=7, description="Day of week")
-    star_time: AwareDatetime = Field(default=None)
-    end_time: AwareDatetime = Field(default=None)
+    price: Decimal | None = Field(default=None, gt=1, description="Price")
+    pricing_id: PricingId | None = Field(default=None)
+    seat_type_id: SeatTypeId | None = Field(default=None)
+    day_of_week: int | None = Field(default=None, ge=1, le=7, description="Day of week")
+    star_time: AwareDatetime | None = Field(default=None)
+    end_time: AwareDatetime | None = Field(default=None)
 
-class PricingItemResponse(PricingItemId, ORMBaseSchema):
+class PricingItemResponse(PricingItemBase, ORMBaseSchema):
     id: PricingItemId
