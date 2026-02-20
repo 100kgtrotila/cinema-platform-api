32f6c43b4cc12ec70bff6cf1cbcff661fdb23dd1
100kgtrotila <hirodevi192@gmail.com>
2026-02-20 18:07:54 +0200

    feat: Extend models with Genre, Hall, and Session classes; enhance Movie and Pricing relationships

--- src/modules/catalog/models.py
+++ src/modules/catalog/models.py
@@ -1,18 +1,23 @@
+from datetime import datetime
+
 from pgvector import Vector
 from sqlalchemy.dialects.postgresql import JSONB
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, Integer, Numeric, Boolean
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, Integer, Numeric, Boolean, DateTime, ForeignKey
 
 from src.common.models import Base
 from decimal import Decimal
 
-from src.modules.catalog.enums import MovieStatus
+from src.modules.catalog.enums import MovieStatus, SessionStatus
+from src.modules.pricing.models import Pricing
 
 
 class Movie(Base):
     __tablename__ = "movies"
 
-    title: Mapped[str] = mapped_column(String)
+    title: Mapped[str] = mapped_column(String(128))
+    slug: Mapped[str] = mapped_column(String(32), nullable=True)
+
     external_id: Mapped[int] | None = mapped_column(Integer, unique=True, nullable=True)
     duration_minutes: Mapped[int] = mapped_column(Integer)
     rating: Mapped[Decimal] = mapped_column(Numeric(3,1))
@@ -26,6 +31,37 @@ class Movie(Base):
     status: Mapped[MovieStatus] = mapped_column(Integer, default=0)
     embedding: Mapped[list[float]] = mapped_column(Vector(768), nullable=True)
 
+    session: Mapped[Session] = relationship(back_populates="movies")
+
+class Genre(Base):
+    __tablename__ = "genres"
+
+    external_id: Mapped[int] = mapped_column(Integer, nullable=True)
+    name: Mapped[str] = mapped_column(String(64))
+    slug: Mapped[str] = mapped_column(String(32), nullable=True)
+
+class Hall(Base):
+    __tablename__ = "halls"
+
+    name: Mapped[str] = mapped_column(String(64))
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    total_capacity: Mapped[int] = mapped_column(Integer)
+
+    session: Mapped[Session] = relationship(back_populates="halls")
+
+class Session(Base):
+    __tablename__ = "sessions"
+
+    start_time: Mapped[datetime] = mapped_column(DateTime(timezone=True))
+    end_time: Mapped[datetime] = mapped_column(DateTime(timezone=True))
+    status: Mapped[SessionStatus] = mapped_column(Integer, default=0)
+    movie_id: Mapped[int] = mapped_column(ForeignKey("movies.id"))
+    hall_id: Mapped[int] = mapped_column(ForeignKey("halls.id"))
+    pricing_id: Mapped[int] = mapped_column(ForeignKey("pricing.id"))
+
+    movies: Mapped[Movie] = relationship(back_populates="sessions")
+    hall: Mapped[Hall] = relationship(back_populates="sessions")
+    pricing: Mapped[Pricing] = relationship(back_populates="sessions")
 
 
 
--- src/modules/pricing/models.py
+++ src/modules/pricing/models.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from decimal import Decimal
 
 from sqlalchemy import String, NUMERIC, Integer, DateTime, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.common.models import Base
 
@@ -12,6 +12,8 @@ class Pricing(Base):
     __tablename__ = "pricing"
 
     name: Mapped[str] = mapped_column(String(150), unique=True)
+
+    session: Mapped[Pricing] = relationship(back_populates="pricing")
 
 class PricingItem(Base):
     __tablename__ = "pricing_items"
@@ -22,3 +24,7 @@ class PricingItem(Base):
     day_of_week: Mapped[int] = mapped_column(Integer)
     start_time: Mapped[datetime] = mapped_column(DateTime(timezone=True))
     end_time: Mapped[datetime] = mapped_column(DateTime(timezone=True))
+
+    pricing: Mapped["Pricing"] = relationship(back_populates="pricing_items")
+    seat_type: Mapped["SeatType"] = relationship(back_populates="seat_types")
+
