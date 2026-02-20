fb45809b415e8b3abc442955f551cd5ef798ba0f
100kgtrotila <hirodevi192@gmail.com>
2026-02-20 17:05:51 +0200

    feat: Add Pricing and PricingItem models to define pricing structure

---
+++ src/modules/pricing/models.py
@@ -0,0 +1,24 @@
+import uuid
+from datetime import datetime
+from decimal import Decimal
+
+from sqlalchemy import String, NUMERIC, Integer
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.common.models import Base
+
+
+class Pricing(Base):
+    __tablename__ = "pricing"
+
+    name: Mapped[str] = mapped_column(String(150), unique=True)
+
+class PricingItem(Base):
+    __tablename__ = "pricing_items"
+
+    price: Mapped[Decimal] = mapped_column(NUMERIC)
+    pricing_id: Mapped[uuid.UUID] = mapped_column(ForeignKey="pricing.id")
+    seat_type_id: Mapped[uuid.UUID] = mapped_column(ForeignKey="seat_types.id")
+    day_of_week: Mapped[int] = mapped_column(Integer)
+    start_time: Mapped[datetime] = mapped_column(DATETIME_TIMEZONE=True)
+    end_time: Mapped[datetime] = mapped_column(DATETIME_TIMEZONE=True)
