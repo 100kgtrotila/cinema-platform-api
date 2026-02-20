46c0641a86a380d2bf2dc29be34378a405084017
100kgtrotila <hirodevi192@gmail.com>
2026-02-20 17:51:06 +0200

    feat: Enhance Movie model with additional fields for cast, deletion status, and embedding

--- src/modules/catalog/models.py
+++ src/modules/catalog/models.py
@@ -1,8 +1,12 @@
+from pgvector import Vector
+from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, Integer, Numeric
+from sqlalchemy import String, Integer, Numeric, Boolean
 
 from src.common.models import Base
 from decimal import Decimal
+
+from src.modules.catalog.enums import MovieStatus
 
 
 class Movie(Base):
@@ -17,5 +21,13 @@ class Movie(Base):
     backdrop_url: Mapped[str] | None = mapped_column(String(500), nullable=True)
     description: Mapped[str] = mapped_column(String)
     release_year: Mapped[int] = mapped_column(Integer)
+    cast: Mapped[list[str]] = mapped_column(JSONB, default=list)
+    id_deleted: Mapped[bool] = mapped_column(Boolean, default=False)
+    status: Mapped[MovieStatus] = mapped_column(Integer, default=0)
+    embedding: Mapped[list[float]] = mapped_column(Vector(768), nullable=True)
+
+
+
+
 
 
